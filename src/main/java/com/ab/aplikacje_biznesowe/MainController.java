ab44d72345d895eb549069377535ab6b0dc614fe
gothdawid <dawidstu@gmail.com>
2022-12-03 12:54:42 +0100

    Dodanie funkcji "o aplikacji"

--- src/main/java/com/ab/aplikacje_biznesowe/MainController.java
+++ src/main/java/com/ab/aplikacje_biznesowe/MainController.java
@@ -2,8 +2,15 @@ package com.ab.aplikacje_biznesowe;
 
 import com.db.api.DbConnection;
 import com.db.api.IDbConnection;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class MainController {
     public TableView<Table> users_table, subjects_table, grades_table, messages_table, groups_table, room_table, schedule_table;
@@ -44,6 +51,26 @@ public class MainController {
         catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    public void about(ActionEvent actionEvent) {
+        //modalne okno z informacjami o aplikacji
+        Stage dialog = new Stage();
+        Label label = new Label("Aplikacja do zarządzania szkołą");
+        Label label2 = new Label("Autorzy: ");
+        Label label3 = new Label("Dawid Studziński");
+        Label label4 = new Label("Joanna Wojtasińska");
+        VBox layout = new VBox(10);
+        //aligment center
+        layout.getChildren().addAll(label, label2, label3, label4);
+        layout.setStyle("-fx-alignment: center");
+
+        dialog.setScene(new Scene(layout, 200, 150));
+        dialog.initOwner(users_table.getScene().getWindow());
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.setTitle("O aplikacji");
+        dialog.showAndWait();
+
     }
 }
 
